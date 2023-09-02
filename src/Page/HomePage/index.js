71b82a608a49295e1fe3fa1f25e28772800caa1f
thanhchi2kbn <thanhchi2kbn@gmail.com>
2023-09-02 10:09:02 +0700

    update animation CallBtn

--- src/Page/HomePage/index.js
+++ src/Page/HomePage/index.js
@@ -5,22 +5,32 @@ import HeaderClient from '../../Layout/HeaderClient'
 import BannerClient from '../../Layout/BannerClient'
 import FooterClient from '../../Layout/FooterClient'
 import BodyClient from '../../Layout/BodyClient'
+import PhoneIcon from '@mui/icons-material/Phone';
 
 export default function Home() {
   return (
     <>
-      <div className='hot-line'>
-      
 
-      </div>
       <div className='home-page'>
         <HeaderClient />
 
         <div className='container'>
           <BannerClient />
           <BodyClient />
+
         </div>
 
+        <div className='hot-line'>
+          <div className='wave-anima1'></div>
+          <div className='wave-anima'></div>
+          <div className='hot-line-icon'>
+            <div className='phone-icon'></div>
+
+
+          </div>
+
+          <div className='hot-line-text'>HOTLINE: 0988123456</div>
+        </div>
         <FooterClient />
       </div>
 
